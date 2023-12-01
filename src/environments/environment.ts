643ec52086bee99c67e90580b37b82bb0f054b39
Hshefat <shefat.hasnain@gmail.com>
2023-12-01 10:38:15 +0600

    services

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+const appStreme = 'app-streme/';
+export const api = 'http://localhost/rms-report-backend/public/api';
+
+export const PPsDomainName = 'http://localhost:4400';
+
+export const environment = {
+    production: true,
+    hmr: false,
+    tusuka: {
+        baseApiEndPoint: appStreme,
+        ppsUrl: 'http://localhost:4400/',
+        ppsRdppRtappBackendPoint: api + appStreme,
+    }
+};
